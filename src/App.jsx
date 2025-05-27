b1c1039b981e965ce3b7a2d468a1728812dbc77e
Michael Asare <55958142+United1l@users.noreply.github.com>
2025-05-27 03:07:36 +0000

    Changed the color of the body. Place the heading component and hero component into a container instead of a fragment in order to give the container a define height and width. Added a section component with a hero text and hero image.

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import Hero from './components/Hero'
 function App() {
 
   return (
-    <>
+    <div className="h-screen w-screen">
       <Header />
       <Hero />
-    </>  
+    </div>  
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,18 @@
+import hero from '../assets/Hero.jpg'
+
 const Hero = () => {
     return (
-        <section className="h-2/5 w-4/5 bg-blue-600">
-            
+        <section className="h-full w-full mr-auto ml-auto mt-24 flex flex-col items-center justify-space-evenly">
+            <div className="hero-text-container w-4/5 md:w-2/3 h-2/5 flex flex-col items-center">
+                <h1 className="hero-text text-[#4CA771] text-4xl md:text-5xl 
+                font-bold text-center ml-auto mr-auto">
+                    Attain quality Medical Supplies with TrustCare MS
+                </h1>
+                <p className="hero-paragraph text-sm font-normal mt-4 text-center md:text-lg text-[#4CA771] italic">
+                    Committed to providing you with the best medical supplies and equipment.
+                </p>
+            </div>
+            <img src={hero} alt="Hero-image" className="h-[90%] w-[80%] md:h-[80%] rounded-lg" />
         </section>
     )
 }
