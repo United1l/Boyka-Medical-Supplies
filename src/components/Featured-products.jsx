91c72fb0f1f01f7f6517ede074dfa6759bd98a54
Michael Asare <55958142+United1l@users.noreply.github.com>
2025-05-28 03:47:55 +0000

    Applied more styles to the images and their containers in the Feautured-products component.

--- src/components/Featured-products.jsx
+++ src/components/Featured-products.jsx
@@ -10,16 +10,19 @@ const FeauturedProducts = () => {
     ];
     
     const renderedImages = images.map((image, index) => (
-        <img key={index} src={image.img} alt={image.alt} className="h-full w-1/3 object-cover" />
-    ));
+        <div key={index} className="featured-container w-[30%] h-4/5 flex justify-center items-center
+            bg-[#4CA771] mr-4 mt-4 rounded-tr-3xl">
+            <img src={image.img} alt={image.alt} className="w-3/4 h-3/4 object-cover" />
+        </div>
+     ));
 
     return (
         <div className="featured-products h-2/5 w-full mt-24 flex flex-col items-center
-            justify-space-evenly">
+            justify-evenly">
             <h2 className="text-3xl text-center text-[#4CA771] font-normal p-8">
                 Featured Products
             </h2>
-            <div className="featured-products-images-container h-3/5 w-[95%] bg-cyan-400 flex flex-wrap">
+            <div className="featured-products-images-container h-3/5 w-[95%] flex flex-wrap justify-center items-center">
                 {renderedImages}             
             </div>
         </div>
