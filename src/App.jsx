49e45791a244b4280b5204fc10442ad2eed9e48f
Michael Asare <55958142+United1l@users.noreply.github.com>
2025-05-30 03:22:30 +0000

    Added a service component that describes the services offered  by Trust care

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header'
 import Hero from './components/Hero'
 import FeauturedProducts from './components/Featured-products'
+import Services from './components/Services'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Hero />
       <FeauturedProducts />
+      < Services />
     </div>  
   )
 }
---
+++ src/components/Services.jsx
@@ -0,0 +1,34 @@
+const Services = () => {
+    const servicesText = [
+        {
+            title: "Expansive Inventory",
+            description: "We offer a wide range of hospital equipment and supplies tailored for your hospital"
+        },
+        {
+            title: "Delivery services",
+            description: "We ensure timely delivery of medical supplies to your hospital"
+        },
+        {
+            title: "24/7 Customer Support",
+            description: "Our customer support team is available 24/7 to assist you with your needs"
+        }
+    ];
+
+    const renderedServices = servicesText.map((service, index) => (
+        <div key={index} className="service-container flex flex-col flex-[0 0 33.33%] items-center 
+            justify-center py-10 mx-4">
+            <h3 className="service-title text-2xl font-bold text-[#4CA771]">{service.title}</h3>
+            <p className="service-description text-sm font-normal mt-2 text-center text-[#6b9080]">
+                {service.description}
+            </p>
+        </div>
+    ));
+
+    return (
+        <div className="services h-2/5 w-full mt-24 flex flex-wrap items-center justify-evenly">
+            {renderedServices}
+        </div>
+    )
+}
+
+export default Services;
