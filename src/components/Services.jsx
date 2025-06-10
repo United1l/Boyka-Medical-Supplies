a111545e1c1fe3b7c8f19445bc1a8eb972897b5f
Michael Asare <55958142+United1l@users.noreply.github.com>
2025-06-10 03:23:38 +0000

    Updating services to look different visually.

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,49 +1,9 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheck } from '@fortawesome/free-solid-svg-icons'
+import TiltedImageContainer from "./TiltedImageContainer" 
 
 const Services = () => {
-    const servicesText = [
-        {
-            title: "Expansive Inventory",
-            description: "We offer a wide range of hospital equipment and supplies tailored for your hospital"
-        },
-        {
-            title: "Delivery services",
-            description: "We ensure timely delivery of medical supplies to your hospital"
-        },
-        {
-            title: "24/7 Customer Support",
-            description: "Our customer support team is available 24/7 to assist you with your needs"
-        }
-    ];
-
-    const checkIconCircle = () => (
-        <div className="w-6 h-6 rounded-full bg-[#4CA771] flex items-center justify-center mr-2">
-            <FontAwesomeIcon icon={faCheck} className="text-white" />
-        </div>
-    ); 
-
-    const renderedServices = servicesText.map((service, index) => (
-        <div key={index} className="service-container flex flex-col flex-[0 0 33.33%] items-center 
-            justify-center py-10 mx-4">
-            <div className="service-icon flex items-center justify-between mb-4">
-                {checkIconCircle()}
-                <h3 className="service-title text-2xl font-bold text-[#4CA771]">{service.title}</h3>
-            </div>
-            <p className="service-description text-sm font-normal mt-2 text-center text-[#6b9080]">
-                {service.description}
-            </p>
-        </div>
-    ));
-
     return (
-        <div className="h-2/5 w-full mt-72 md:mt-16 flex flex-col items-center justify-center">
-            <h1 className="text-4xl text-[#4CA771] font-bold">
-                Our Services
-            </h1>
-            <div className="services h-4/5 w-full mt-8 flex flex-wrap items-center justify-evenly">
-                {renderedServices}
-            </div>
+        <div className="h-4/5 w-full mt-84 md:mt-16 flex flex-col">
+            
         </div>
     )
 }
