6cbcb61530e3145593008085e1ac2d6e7878ecdb
Michael Asare <55958142+United1l@users.noreply.github.com>
2025-06-14 03:07:06 +0000

    Edited the style the images in the services component were presented in.

--- src/components/TiltedImageContainer.jsx
+++ src/components/TiltedImageContainer.jsx
@@ -1,10 +1,8 @@
 const TiltedImageContainer = ({ sourceImage, altText }) => {
   return (
-    <>
-      <div className="w-full md:w-1/3 h-auto rounded-lg">
-        <img src={sourceImage} alt={altText} className="w-full h-full" />
-      </div>
-    </>  
+    <div className="w-full md:w-1/3 h-auto rounded-lg rotate-9 bg-linear-to-r from-[#95d5b2] to-[#2b9348]">
+      <img src={sourceImage} alt={altText} className="w-full h-full rotate-350" />
+    </div>  
   )
 }
 
