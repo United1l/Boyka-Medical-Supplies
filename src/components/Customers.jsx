8f12a60b9e0218f7969d75111f9000dc81307e1c
Michael Asare <55958142+United1l@users.noreply.github.com>
2025-06-23 15:04:55 +0000

    Gave an animation sequence to the items (logos) inside the slider container that makes it move from the right side of the screen to the left out of view and then the same motion is repeated infinitely. It was done inside the moveImages component. @ keyframes rule was used. It was finally used in the customers component.

--- src/components/Customers.jsx
+++ src/components/Customers.jsx
@@ -1,4 +1,24 @@
+import MoveImages from './MoveImages'
+import military from '../assets/customers/37military.jpg'
+import cityhospital from '../assets/customers/city-hospital.jpg'
+import westernpharma from '../assets/customers/western-pharmacy.jpg'
+import sandybay from '../assets/customers/sandybay-pharmacy.jpg'
+import korlebu from '../assets/customers/korlebu.webp'
+import kath from '../assets/customers/kath-logo.webp'
+import ernestchemist from '../assets/customers/ernest-chemist.png'
+
+
 const Customers = () => {
+  const images = [
+    { src: military, alt: 'Military Hospital' },
+    { src: cityhospital, alt: 'City Hospital' },
+    { src: westernpharma, alt: 'Western Pharmacy' },
+    { src: sandybay, alt: 'Sandy Bay Pharmacy' },
+    { src: korlebu, alt: 'Korle Bu Teaching Hospital' },
+    { src: kath, alt: 'Kumasi Teaching Hospital' },
+    { src: ernestchemist, alt: 'Ernest Chemist' }
+  ];
+
   return (
     <div className="w-full h-auto flex flex-col items-center mt-16">
       <h2 className="text-4xl text-center text-[#4CA771] font-bold p-8">
@@ -7,6 +27,7 @@ const Customers = () => {
       <p className="text-[#343a40] text-lg text-center tracking-wide px-4 md:px-16">
         Hospitals and Pharmacies that use our services
       </p>
+      <MoveImages images={images} />
     </div>
   )
 }
--- src/components/MoveImages.jsx
+++ src/components/MoveImages.jsx
@@ -1,13 +1,16 @@
 const MoveImages = ({ images }) => {
   const renderedImages = images.map((image, index) => (
-    <div key={index} className="h-auto">
+    <div key={index} className="item size-[100px] absolute left-[100%] animate-[autoRun_10s_linear_infinite]"
+      style={{ animationDelay: `${index}s` }}>
+      {/* The absolute positioning and animation will move the images */}
+      {/* from right to left across the screen */}
       <img src={image.src} alt={image.alt} className="h-full" />
     </div>
   ))
 
   return (
-    <div className="w-full h-[100px]">
-      <div className="w-full flex">
+    <div className="w-full h-[100px] mt-8">
+      <div className="w-full flex justify-between relative">
         {renderedImages}
       </div>
     </div>
