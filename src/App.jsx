c4abca9f59423c6c7e716e3165b60549bed2afea
Michael Asare <55958142+United1l@users.noreply.github.com>
2025-07-07 17:23:36 +0000

    Added a dummy text to the shop component whilst adding the routes to App,jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { Routes, Route } from 'react-router'
 import Home from './components/Home'
+import Shop from './components/Shop'
 
 function App() {
 
   return (
     <div>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
     </div>  
   )
 }
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,7 +1,8 @@
 const Shop = () => {
   return (
-    <div>
-        <h1>Shop</h1>
+    <div className="flex flex-col items-center justify-center h-screen w-screen text-green-500">
+        <h1 className="text-4xl">Shop</h1>
+        <p>The items will go here</p>
     </div>
   )
 }
