f114304a5b1c596cfde42c04665d91af40c5ae33
Michael Asare <55958142+United1l@users.noreply.github.com>
2025-07-10 03:16:26 +0000

    Created a general direction of  where the shop page is going.

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,8 +1,11 @@
 const Shop = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen w-screen text-green-500">
-        <h1 className="text-4xl">Shop</h1>
-        <p>The items will go here</p>
+    <div className="h-screen w-screen text-green-500">
+        <div className="w-full h-auto flex items-center justify-evenly text-4xl font-semibold p-4">
+          <h2 className="border-b-4 border-green-500">Drugs</h2>
+          <h2>Non Drugs</h2>
+          <h2>Equipment</h2>
+        </div>
     </div>
   )
 }
