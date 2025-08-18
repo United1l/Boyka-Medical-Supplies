c4aa8b4217d87cfc5140e66522c9a2f7bdacba9d
Michael Asare <55958142+United1l@users.noreply.github.com>
2025-08-18 12:43:39 +0000

    Changed the variable names in the env file to better match the vite bundler being used and to properly import it in the supabaseClient file in order to properly expose it to the clientside in the browser when rendering the site. Then finally fetched the data from the supabase client. The data was then rendered according to its category namely name, weight and price.

--- src/components/Drugs.jsx
+++ src/components/Drugs.jsx
@@ -1,7 +1,47 @@
+import supabase from '../config/supabaseClient'
+import { useState, useEffect } from 'react'
+
 const Drugs = () => {
+  const [drugs, setDrugs] = useState([]);
+  const [fetchError, setFetchError] = useState(null);
+
+  useEffect(() => {
+    const fetchDrugs = async () => {
+      const { data, error } = await supabase
+        .from('Drugs')
+        .select('*');
+        
+      if (error) {
+        setFetchError('Could not fetch drugs');
+        setDrugs([]);
+      } else {
+        setDrugs(data);
+        setFetchError(null);
+      }
+    };
+
+    fetchDrugs();
+
+  }, []);  
+
   return (
     <div className="w-full h-full flex items-center justify-center">
-      <h1 className="text-3xl text-green-400">Drugs</h1>
+      {fetchError && <p className="text-red-500">{fetchError}</p>}
+      {drugs && <div className="w-3/4 h-auto flex flex-col justify-evenly p-10">
+        <div className="w-full h-auto flex flex-wrap justify-between items-center p-4 text-lg font-bold">
+          <h1>NAME</h1>
+          <h1>WEIGHT</h1>
+          <h1>PRICE</h1>
+        </div>
+        {drugs.map((drug, index) => (
+          <div key={index} className="w-full h-auto flex flex-wrap justify-between items-center p-4">
+            <h1>{drug.name}</h1>
+            <h1>{drug.weight}</h1>
+            <h1>{drug.price}</h1>
+          </div>
+        ))}  
+      </div>
+      }    
     </div>
   )
 }
--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,6 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
+// This file initializes the Supabase client with the URL and Anon Key from environment variables.
+// Ensure that these environment variables are set in your .env file or hosting environment.
+// The client can be used to interact with your Supabase database and authentication services.
